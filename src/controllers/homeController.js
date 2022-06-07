943a2652983f5754274815e1676dd89da01c7de6
VDimov01 <vdimov54@gmail.com>
2022-06-07 15:51:25 +0300

    used api for homepage

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
 const cubeService = require('../services/cubeService');
+const fetch = require('node-fetch');
 
 router.get('/', async (req, res) => {
-    let {search, from, to} = req.query;
-    const cubes = await cubeService.getAll(search, Number(from), Number(to));
+    // let {search, from, to} = req.query;
+    // const cubes = JSON.parse(await cubeService.getAll(search, Number(from), Number(to)));
+    // res.render('index', {cubes, search, from, to});
+
+    const response = await fetch('http://localhost:5000/api/cubes');
+    const cubes = await response.json();
     
-    res.render('index', {cubes, search, from, to});
+    res.render('index', {cubes});
+
 });
+
+// router.get('/ap', async (req, res) => {
+//     let response = await fetch('http://localhost:5000/api/cubes')
+//     let data = await response.json();
+
+//     res.send(data);
+// });
 
 router.get('/about', (req, res) => {
     res.render('about');
